1a2eeb1a5608879ef6de7208626ed1623bda05d4
icaro espadim <10087971@pucrs.br>
2022-08-30 21:53:35 -0300

    regra de negocio quase ok

--- src/main/java/GerenciaLugares.java
+++ src/main/java/GerenciaLugares.java
@@ -1,40 +1,66 @@
 public class GerenciaLugares {
     // [fileiras][lugares]
-    public boolean[][] lugares = new boolean[60][20];
-
+    public boolean[][] lugares = new boolean[61][21];
     public boolean[][] getLugares() {
         return lugares;
     }
+    public int passageiros;
 
     public int verificaLugar(String fila_assento) {
-        int fila = new Integer(fila_assento.substring(1,2));
-        int assento = new Integer(fila_assento.substring(3,4));
-
-        switch(verificaLugar(fila,assento)){
-            case 2:
-                break;
-        }
-
+        int fila = new Integer(fila_assento.substring(1,3));
+        int assento = new Integer(fila_assento.substring(4,6));
+        System.out.println("F"+fila +"A"+assento);
+        return verificaLugar(fila,assento);
     }
 
-    public boolean verificaLugar(int fila, int assento) {
+    private boolean checa(int fila, int assento) {
         return lugares[fila][assento];
     }
 
+    private int verificaLugar(int fila, int assento) {
+        //0 out of bounds
+        if(fila >60 | assento > 20){
+            return 0;
+        }else{
+            //1 assento ocupado
+            if(lugares[fila][assento]){
+                return 1;
+            }
+
+            //3 assento livre
+            //3.1 Os primeiros 100 passageiros só podem se sentar nas fileiras de 1 a 20.
+            if(passageiros<100 & fila<20){
+                lugares[fila][assento] = true;
+                passageiros++;
+                return 3;
+            }
+            //3.2 Os próximos 100 passageiros só podem se sentar nas fileiras de 40 a 60.
+            if (passageiros>=100 & passageiros<200 & fila>40 & fila <=60 ){
+                lugares[fila][assento] = true;
+                passageiros++;
+                return 3;
+            }
+            return 2;
+        }
+    }
+
     public void desenhaBarca() {
-        for (int i = 0; i < 60; i++) {
+        for (int i = 1; i < 61; i++) {
             if(i<10){
                 System.out.print("F0" + i+" ");
             }else{
                 System.out.print("F" + i+" ");
             }
-            for (int j = 0; j < 20; j++) {
-                boolean lugar = verificaLugar(i, j);
+            for (int j = 1; j < 21; j++) {
+                boolean lugar = checa(i, j);
+                //System.out.print("["+i+"]"+"["+j+"]: ");
                 if (lugar) {
-                    System.out.print("o ");
+                    //assento OCUPADO
+                    System.out.print("1 ");
                 } else {
-                    System.out.print("v ");
+                    System.out.print("0 ");
                 }
+                //System.out.print("  ");
             }
             System.out.println();
         }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,13 @@ public class Main {
 
     public static void main(String args[]) {
         GerenciaLugares bilheteria = new GerenciaLugares();
-        bilheteria.desenhaBarca();
 
+        System.out.println( bilheteria.verificaLugar("F03A19"));
+        System.out.println( bilheteria.verificaLugar("F21A19"));
+        bilheteria.passageiros=100;
+        System.out.println( bilheteria.verificaLugar("F41A19"));
+
+
+        bilheteria.desenhaBarca();
     }
 }
